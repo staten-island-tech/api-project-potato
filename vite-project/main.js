7dc9c92a41ee7fbda469fcb2836919c0c0c13e21
DanielsPotatos <JOnathanw26@nycstudents.net>
2021-12-13 23:52:22 -0500

    ss

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,10 +1,41 @@
 import './style.css'
 
-const URL = "http://api.icndb.com/jokes/random";
+// API (Application Programming Interface) - if you want to get data
+// from the internet programmatically, it's likely that you will have
+// to deal with an API.
 
-async function getData(URL){
+// Below is an entry point to an api that generates random quotes.
+// APIs are usually accessible with a HTML/website link.
+// (If you open this in a browser, you will get raw object data.)
+const apiEntry = "https://api.chucknorris.io/jokes/random";
 
-  const response = await fetch(URL);
-  const data = await response.json();
+// fetch is a function (that you've seen previously) that can retrieve
+// data from an api entry point.
+console.log(fetch(apiEntry));
+
+// fetch() returns a "response", which we must convert into a object json format
+fetch(apiEntry)
+    .then((response) => response.json()) // use the .json() method
+    .then((data) => console.log(data)); // .json() is also async, chain another .then() to log the object
+
+// let's turn this to an async/await function
+async function fetchData(apiEntry) {
+    try {
+        const response = await fetch(apiEntry);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
 }
-getData(URL)
+fetchData(apiEntry);
+
+// paired with DOM selectors, you can display dynamic data onto your HTML!
+const apiResponseDOM = document.getElementById("api-response");
+const putQuoteInHTML = async () => {
+    // defining an async arrow function
+    const quote = await fetchData(apiEntry);
+    apiResponseDOM.innerHTML = `Quote: ${quote.value}`;
+};
+putQuoteInHTML();
